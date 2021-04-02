3d5cd19a4938ef428e1aed93ec621e4bbee658b8
sspate26 <78052270+sspate26@users.noreply.github.com>
2021-04-02 09:14:08 -0700

    Update Ingredients.sql

--- Ingredients.sql
+++ Ingredients.sql
@@ -9,10 +9,10 @@ CREATE TABLE ingredients (
 INSERT INTO ingredients
     (ingredient_id, ingredient_name, ingredient_price)
 VALUES 
-    (1, "Beef", 5),
-    (2, "Lettuce", 1),
-    (3, "Tomatoes", 2),
-    (4, "Taco Shell", 2),
-    (5, "Cheese", 3),
-    (6, "Milk", 1),
-    (7, "Bread", 2);
+    (1, 'Beef', 5),
+    (2, 'Lettuce', 1),
+    (3, 'Tomatoes', 2),
+    (4, 'Taco Shell', 2),
+    (5, 'Cheese', 3),
+    (6, 'Milk', 1),
+    (7, 'Bread', 2);
